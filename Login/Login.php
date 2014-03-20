12488fe3b44482eb11703cdbd352703252ac57b8
hiiamsky <hi.iamsky@gmail.com>
2014-03-20 20:18:03 +0800

    html更正為class方式

--- Login/Login.php
+++ Login/Login.php
@@ -1,6 +1,7 @@
 <?php
 	include '../function/html.php';
-	
+	$html=new html();
+	//設定Login.php用變數
 	$titleStr="登入";
 	$JSLinkStr="<script src=\"../JS/Login/Login.js\"></script>\n";
 	$CSSStr="";
@@ -9,23 +10,23 @@
 	
 	$headercontent="<h1>登入</h1>\n";
 	$headerdivotherstr=" date-theme=\"".$data_theme."\"";
-	$header=jQueryMobileHeader($headercontent, $headerdivotherstr);
+	$header=$html->jQueryMobileHeader($headercontent, $headerdivotherstr);
 	
 	$jMcontent="";
 	$jMcontent.="<form name=\"LoginForm\" id=\"LoginForm\" method=\"post\" action=\"ckLogin.php\">\n";
-	$jMcontent.= jQueryM_Text("BnbID","BnbID","民宿編號","","");
-	$jMcontent.= jQueryM_Text("LoginID","LoginID","使用者名稱","","");
-	$jMcontent.= jQueryM_PWText("LoginPW","LoginPW","使用者密碼","","");
-	$jMcontent.= jQueryM_Button("btnSumit","btnSumit","button","送出"," data-theme=\"".$data_theme."\"");
+	$jMcontent.= $html->jQueryM_Text("BnbID","BnbID","民宿編號","","");
+	$jMcontent.= $html->jQueryM_Text("LoginID","LoginID","使用者名稱","","");
+	$jMcontent.= $html->jQueryM_PWText("LoginPW","LoginPW","使用者密碼","","");
+	$jMcontent.= $html->jQueryM_Button("btnSumit","btnSumit","button","送出"," data-theme=\"".$data_theme."\"");
 	$jMcontent.="</form>\n";
-	$content=jQueryMobileContent($jMcontent, "");
+	$content=$html->jQueryMobileContent($jMcontent, "");
 	
 	$footcontent="<h4>bnb</h4>\n";
-	$footer=jQueryMobileFooter($footcontent, "");
+	$footer=$html->jQueryMobileFooter($footcontent, "");
 	
-	echo htmlHead($titleStr, $CSSStr, $JSLinkStr);	
-	echo jQueryMobilePage($pageID, $header, $content, $footer, "");
-	echo htmlEnd();
+	echo $html->htmlHead($titleStr, $CSSStr, $JSLinkStr);	
+	echo $html->jQueryMobilePage($pageID, $header, $content, $footer, "");
+	echo $html->htmlEnd();
 	
 ?>
 
