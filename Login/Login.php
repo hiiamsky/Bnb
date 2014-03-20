254a853fe157d7ca04366fd878d3371dac11fe1c
hiiamsky <hi.iamsky@gmail.com>
2014-03-20 19:46:45 +0800

    清出多餘程式碼

--- Login/Login.php
+++ Login/Login.php
@@ -27,28 +27,5 @@
 	echo jQueryMobilePage($pageID, $header, $content, $footer, "");
 	echo htmlEnd();
 	
-	
-	
-	
-	
-		// echo "<div data-role=\"page\" id=\"page\" id=\"LoginPage\">\n";
-	// echo "<div data-role=\"header\" date-theme=\"b\">\n";
-	// echo "<h1>登入</h1>\n";
-	// echo "</div>\n";
-	// echo "<div data-role=\"content\">\n";
-	// echo "<form name=\"LoginForm\" id=\"LoginForm\" method=\"post\" action=\"\">\n";
-// 
-	// echo jQueryM_Text("BnbID","BnbID","民宿編號","","");
-	// echo jQueryM_Text("LoginID","LoginID","使用者名稱","","");
-	// echo jQueryM_PWText("LoginPW","LoginPW","使用者密碼","","");
-	// echo jQueryM_Button("btnSumit","btnSumit","button","送出"," data-theme=\"b\"");		
-// 				
-	// echo "</form>\n";
-// 			
-	// echo "</div>\n";
-	// echo "<div data-role=\"footer\">\n";
-	// echo "<h4>bnb</h4>\n";
-	// echo "</div>\n";
-	// echo "</div>\n";
 ?>
 
