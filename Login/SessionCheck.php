bed949bad2b06aaa87f19681aad27b4362676476
hiiamsky <hi.iamsky@gmail.com>
2014-03-21 13:41:14 +0800

    寫入註解

--- Login/SessionCheck.php
+++ Login/SessionCheck.php
@@ -1,6 +1,6 @@
 <?php	
-	if(!isset($_SESSION['BnbID']) && !isset($_SESSION['LoginID'])){
-		//echo NOW_DIR."/../Login/Login.php";
+	//判斷是否有登入
+	if(!isset($_SESSION['BnbID']) && !isset($_SESSION['LoginID'])){		
 		header("location: Login/Login.php");
 		exit();		
 	}
