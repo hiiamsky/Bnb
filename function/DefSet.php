66f9d5840c715d4b4c128873b95c489e8356de18
hiiamsky <hi.iamsky@gmail.com>
2014-03-23 17:02:19 +0800

    常數設定PHP

---
+++ function/DefSet.php
@@ -0,0 +1,6 @@
+<?php
+	 define("DSN","mysql:host=localhost;port=3306;dbname=");
+	 define("DB_USERNM","bnbadmin");
+	 define("DB_PW","sky_Bnb047");
+	 define("DB_UTF8","SET CHARACTER SET utf8");
+?>
