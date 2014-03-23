774871fcf5d078dd9be1d7b4ea417dddc3b981e0
hiiamsky <hi.iamsky@gmail.com>
2014-03-23 15:49:30 +0800

    再次改成 $.moble.cheangPage的方式,因為之前的 content的模式不符合 jQuery Mobile

--- JS/Login/Login.js
+++ JS/Login/Login.js
@@ -1,11 +1,6 @@
 
 $(document).ready(function(){
-      $.ajaxPrefilter( function(options, originalOptions, jqXHR) {
-            if ( applicationCache && applicationCache.status != applicationCache.UNCACHED && applicationCache.status != applicationCache.OBSOLETE ) {
-                  // the important bit
-                  options.isLocal = true;
-            }
-      });
+
 	$(document).on("click","#btnSumit",function(evt){
 		var loginData=$("#LoginForm").serialize();		
 		$.ajax({
@@ -32,15 +27,14 @@ $(document).ready(function(){
   			success:function(msg){
   				msg=$.trim(msg);
   				if(msg=="TRUE"){
-  					// $.mobile.changePage('../BnbMenu.php');
-  					document.location.href="../BnbMenu.php";
+  					$.mobile.changePage('../BnbMenu.php');
+  					// document.location.href="../BnbMenu.php";
   				}else{
   					alert("登入失敗,請檢查民宿編號,使用者名稱,使用者密碼是否有誤!!!");
   				}
   			},
   			error: function(jqXHR,textStatus,errorThrown){
-  				alert("登入發生失敗,請聯絡網站管理人員!!!");
-  				
+  				alert("登入發生失敗,請聯絡網站管理人員!!!");  				
   			}
 		});
 		
