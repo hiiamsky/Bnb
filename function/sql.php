d5edee34803b3807e36ab3548cf84072f87fd1ec
hiiamsky <hi.iamsky@gmail.com>
2014-03-25 14:48:30 +0800

    寫入其他的function

--- function/sql.php
+++ function/sql.php
@@ -22,6 +22,29 @@ class sql{
 			$this->stmt=$this->dbh->prepare($query);			
 			
 		}
+		
+		public function bind($param, $value, $type = null){
+    		if (is_null($type)) {
+		        switch (true) {
+		            case is_int($value):
+		                $type = PDO::PARAM_INT;
+		                break;
+		            case is_bool($value):
+		                $type = PDO::PARAM_BOOL;
+		                break;
+		            case is_null($value):
+		                $type = PDO::PARAM_NULL;
+		                break;
+		            default:
+		                $type = PDO::PARAM_STR;
+		        }
+    		}
+    		$this->stmt->bindValue($param, $value, $type);
+		}
+		public function execute(){
+   			return $this->stmt->execute();
+		}
+		
 		//關閉連線
 		public function Close(){
 			$this->dbh=NULL;
